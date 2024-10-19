f9d415bf7ab5bd68f15949737a8accbdaf3a3168
Ahmed Rafi Ullah <ahmed@windscribe.com>
2024-10-19 16:08:52 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,51 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import "./firebase.ts";
 import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
-  signOut,
-  User,
+  UserCredential,
 } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
 
+// This code runs inside of an iframe in the extension's offscreen document.
+// This gives you a reference to the parent frame, i.e. the offscreen document.
+// You will need this to assign the targetOrigin for postMessage.
+const PARENT_FRAME = document.location.ancestorOrigins[0];
+
+function sendResponse(result: UserCredential | Error) {
+  globalThis.parent.self.postMessage(JSON.stringify(result), PARENT_FRAME);
+}
+
 function App() {
-  const [user, setUser] = useState<User | null>(null);
+  // const [user, setUser] = useState<User | null>(auth.currentUser);
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const listener = ({ data }: { data: any }) => {
+      if (data.initAuth) {
+        // Opens the Google sign-in page in a popup, inside of an iframe in the
+        // extension's offscreen document.
+        // To centralize logic, all respones are forwarded to the parent frame,
+        // which goes on to forward them to the extension's service worker.
+        signInWithPopup(auth, provider).then(sendResponse).catch(sendResponse);
+      }
+    };
+    globalThis.addEventListener("message", listener);
+
+    return () => {
+      globalThis.removeEventListener("message", listener);
+    };
+  });
+
+
   return (
     <>
-      <h1>Login below</h1>
-      {!user && (
+      {/* <h1>Login below</h1>
+      {/* {!user && (
         <button
           onClick={() => {
             signInWithPopup(auth, provider)
@@ -73,7 +101,7 @@ function App() {
       )}
 
       <h2>User Data</h2>
-      <pre>{JSON.stringify(user, undefined, 4)}</pre>
+      <pre>{JSON.stringify(user, undefined, 4)}</pre> */} */}
     </>
   );
 }
